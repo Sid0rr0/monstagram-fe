0cd7759f429ff5eb83a18fb16e72302481b9bcf1
Thanh Hung Le <elhungus@gmail.com>
2022-11-29 11:28:57 +0100

    Added: inputs

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
   <main class="homepage m-auto">
-    <div />
+    <h1 class="text-5xl mt-10">
+      Analyzujte vaše sledující na Instagramu
+    </h1>
+
+    <div class="flex flex-col mt-56 w-6/12 m-auto">
+      <div class="flex justify-between items-center">
+        <span>Instagram handle:</span>
+        <input type="text" class="mt-3 border-2">
+      </div>
+      <div class="flex justify-between items-center">
+        <span>Session ID:</span>
+        <input type="text" class="mt-3 border-2">
+      </div>
+    </div>
   </main>
 </template>
 
